ebb8e83c13bb88e72d29a60650a1ac0e27293920
Samir Soormally <samirsoormally@gmail.com>
2019-01-07 14:26:48 +0000

    feat: adds custom size map to World

--- lib/world.rb
+++ lib/world.rb
@@ -1,8 +1,11 @@
 class World
-  attr_reader :rows, :columns
+  attr_reader :rows, :columns, :map
   def initialize(rows, columns)
     @rows = rows
     @columns = columns
-    # @map = []
+    @map = Array.new(rows) do |r|
+            Array.new(columns) do |c|
+            end
+          end
   end
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/world.rb'
 describe 'World' do
   subject(:world) {World.new(5, 5)}
+  subject(:small_world) {World.new(3, 3)}
 
   it "knows it\'s no. rows" do
     expect(world).to respond_to(:rows)
@@ -8,7 +9,18 @@ describe 'World' do
   it "knows it\'s no. columns" do
     expect(world).to respond_to(:columns)
   end
-    # it "starts with empty array" do
-    #   expect(world.map).to eq([])
-    # end
+  it "creates a map corresponding to arguments given" do
+    expect(world.map).to eq([
+      [nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil]
+    ])
+    expect(small_world.map).to eq([
+      [nil, nil, nil],
+      [nil, nil, nil],
+      [nil, nil, nil]
+    ])
+  end
 end
