869ca861ef1e9b1f3546c3d63d4f129c91d87ae3
Samir Soormally <samirsoormally@gmail.com>
2019-01-15 11:11:27 +0000

    fix: adds location to cells (miss-saved)

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,6 +1,8 @@
 class Cell
-  attr_reader :living
-  def initialize
+  attr_reader :living, :loc_x, :loc_y
+  def initialize(loc_x = 0, loc_y = 0)
     @living = false
+    @loc_x = loc_x
+    @loc_y = loc_y
   end
 end
