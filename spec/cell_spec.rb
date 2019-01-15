916b250403d4cd0eb73fea45c542d3fa88e69168
Samir Soormally <samirsoormally@gmail.com>
2019-01-15 11:10:24 +0000

    feat: adds location to cells

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,8 +2,14 @@ require_relative '../lib/cell.rb'
 
 describe 'Cell' do
   subject(:cell) {Cell.new}
+  subject(:mid_cell) {Cell.new(4, 4)}
 
   it "initiazlises dead" do
     expect(cell.living).to eq(false)
+  end
+
+  it "knows it's location" do
+    expect(mid_cell.loc_x).to eq(4)
+    expect(mid_cell.loc_y).to eq(4)
   end
 end
