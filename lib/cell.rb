fbc407d0d9e8b5c52c50ff5a082630d72383bed6
Samir Soormally <samirsoormally@gmail.com>
2019-02-02 11:13:01 +0000

    refactor: abstract access to Cell instance variables via methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,20 @@
 class Cell
-  attr_reader :living, :next_living, :loc_x, :loc_y
   def initialize(loc_x = 0, loc_y = 0)
     @living = false
     @next_living = false
     @loc_x = loc_x
     @loc_y = loc_y
+  end
+
+  def living?
+    @living
+  end
+
+  def next_living?
+    @next_living
+  end
+
+  def show_location
+    [@loc_x][@loc_y]
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,12 +5,11 @@ describe 'Cell' do
   subject(:mid_cell) {Cell.new(4, 4)}
 
   it "initiazlises dead" do
-    expect(cell.living).to eq(false)
-    expect(cell.next_living).to eq(false)
+    expect(cell.living?).to eq(false)
+    expect(cell.next_living?).to eq(false)
   end
 
-  it "knows it's location" do
-    expect(mid_cell.loc_x).to eq(4)
-    expect(mid_cell.loc_y).to eq(4)
+  it "shows cell location" do
+    expect(mid_cell.show_location).to eq([4][4])
   end
 end
