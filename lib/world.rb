1f039358d871adaf66025b68d7edc4740aacb804
Samir Soormally <samirsoormally@gmail.com>
2019-02-03 10:27:06 +0000

    refactor: extracted cell neighbour locations into own method

--- lib/world.rb
+++ lib/world.rb
@@ -1,6 +1,7 @@
 
 class World
   attr_reader :rows, :columns, :map
+
   def initialize(rows, columns)
     @rows = rows
     @columns = columns
@@ -15,20 +16,17 @@ class World
     living_neighbours = 0
     @map.each do |array|
       array.each do |element|
-        living_neighbours += 1 if ((element.show_location == [x-1, y-1]) && (element.living?))
-        living_neighbours += 1 if ((element.show_location == [x-1, y]) && (element.living?))
-        living_neighbours += 1 if ((element.show_location == [x-1, y+1]) && (element.living?))
-        living_neighbours += 1 if ((element.show_location == [x-0, y-1]) && (element.living?))
-        living_neighbours += 1 if ((element.show_location == [x-0, y+1]) && (element.living?))
-        living_neighbours += 1 if ((element.show_location == [x+1, y-1]) && (element.living?))
-        living_neighbours += 1 if ((element.show_location == [x+1, y]) && (element.living?))
-        living_neighbours += 1 if ((element.show_location == [x+1, y+1]) && (element.living?))
+        living_neighbours += 1 if neighbour_locations(x,y).include?(element.show_location) && (element.living?)
       end
     end
     living_neighbours
   end
 
+private
+
+  def neighbour_locations(x, y)
+    [[x-1, y-1], [x-1, y], [x-1, y+1], [x-0, y-1], [x-0, y+1], [x+1, y-1], [x+1, y], [x+1, y+1]]
+  end
 # live cell with 2/3 survives else dead
 # dead cell with 3 is alive else dead
-
 end
