1f0a4a4c1c1bd51a96c01bc823b31a4883082b2b
Samir Soormally <samirsoormally@gmail.com>
2019-02-04 16:38:38 +0000

    refactor: removed redundant dimension instance variables

--- lib/world.rb
+++ lib/world.rb
@@ -3,8 +3,6 @@ class World
   attr_reader :rows, :columns, :map
 
   def initialize(rows, columns)
-    @rows = rows
-    @columns = columns
     @map = Array.new(rows) do |r|
             Array.new(columns) do |c|
               Cell.new(r, c)
