2cfc33194797f6613f3f99297a8f487fda14e913
Samir Soormally <samirsoormally@gmail.com>
2019-02-06 22:46:09 +0000

    refactor: extracted most tick method logic to cells_to_change method

--- lib/world.rb
+++ lib/world.rb
@@ -10,22 +10,22 @@ class World
   end
 
   def tick
-    toggle = []
-    @map.each do |array|
-      array.each do |element|
-        living_neighbours = find_living_neighbours(element.location[0], element.location[1])
-        if (living_neighbours != (2 || 3)) && (element.living? == true)
-          toggle << element
-        end
-        if (living_neighbours == 3) && (element.living? == false)
-          toggle << element
-        end
-      end
-    end
-    toggle.each { |x| x.living_toggle }
+    cells_to_change.each { |x| x.living_toggle }
   end
 
 private
+
+  def cells_to_change
+    cells = []
+    @map.each do |array|
+      array.each do |element|
+        living_neighbours = living_neighbours(element.location[0], element.location[1])
+        cells << element if (living_neighbours != (2 || 3)) && (element.living? == true)
+        cells << element if (living_neighbours == 3) && (element.living? == false)
+      end
+    end
+    cells
+  end
 
   def neighbour_locations(x_axis, y_axis)
     [[x_axis - 1, y_axis - 1], [x_axis - 1, y_axis], [x_axis - 1, y_axis + 1],
@@ -33,7 +33,7 @@ private
      [x_axis + 1, y_axis - 1], [x_axis + 1, y_axis], [x_axis + 1, y_axis + 1]]
   end
 
-  def find_living_neighbours(x_axis, y_axis)
+  def living_neighbours(x_axis, y_axis)
     living_neighbours = 0
     @map.each do |array|
       array.each do |element|
